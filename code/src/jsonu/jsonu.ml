024c3cfe6233dc5d05f35fb246670985664e446b
Tamiya <x-access-token@users.noreply.github.com>
2026-08-18 15:26:30 +0200

    monorepo->OSS export 2026-08-18 14:08:03 UTC
    GitOrigin-RevId: b7a03ddb1fc73af511318495b2846db219f5ba44

--- code/src/jsonu/jsonu.ml
+++ code/src/jsonu/jsonu.ml
@@ -13,7 +13,7 @@ let of_yaml_string yaml_str =
   | Ok json_str -> Ok (Yojson.Safe.from_string json_str)
   | Error err -> Error (`Yaml_decode_err err)
 
-let rec merge' ~null_is_identity ~list_append ~base override =
+let rec merge' ~null_is_identity ~list ~base override =
   match (base, override) with
   | `Bool _, (`Bool _ as v)
   | `Intlit _, (`Intlit _ as v)
@@ -22,8 +22,10 @@ let rec merge' ~null_is_identity ~list_append ~base override =
   | `Int _, (`Intlit _ as v)
   | `Float _, (`Float _ as v)
   | `String _, (`String _ as v) -> Ok v
-  | `List b, `List o ->
-      if list_append then Ok (`List (CCList.append b o)) else Ok (`List (CCList.append o b))
+  | `List b, `List o -> (
+      match list with
+      | `Prepend -> Ok (`List (CCList.append o b))
+      | `Append -> Ok (`List (CCList.append b o)))
   | (`Assoc b as base), `Assoc o ->
       let open CCResult.Infix in
       CCResult.fold_l
@@ -31,7 +33,7 @@ let rec merge' ~null_is_identity ~list_append ~base override =
           match Yojson.Safe.Util.member k base with
           | `Null -> Ok ((k, v) :: acc)
           | v' -> (
-              match merge' ~null_is_identity ~list_append ~base:v' v with
+              match merge' ~null_is_identity ~list ~base:v' v with
               | Ok v -> Ok ((k, v) :: acc)
               | Error (`Type_mismatch_err (None, b, o)) -> Error (`Type_mismatch_err (Some k, b, o))
               | Error (`Type_mismatch_err (Some p, b, o)) ->
@@ -55,7 +57,7 @@ let rec merge' ~null_is_identity ~list_append ~base override =
   | `Variant _, _ | _, `Variant _ -> assert false
   | b, o -> Error (`Type_mismatch_err (None, b, o))
 
-let merge ?(null_is_identity = false) ?(list_append = false) ~base override =
-  (merge' ~null_is_identity ~list_append ~base override
+let merge ?(null_is_identity = false) ?(list = `Prepend) ~base override =
+  (merge' ~null_is_identity ~list ~base override
     : (Yojson.Safe.t, merge_err) result
     :> (Yojson.Safe.t, [> merge_err ]) result)
